d31d71a2a5bf40c664b43d36a38297e803238023
shalini-90 <shalini.hyperloop@gmail.com>
2025-03-11 11:35:16 +0530

    First commit

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,22 @@
+//import { expect, browser, $ } from '@wdio/globals'
+
+const { expect } = require('@wdio/globals');
+const browser = require('@wdio/globals').browser;
+const $ = require('@wdio/globals').$;
+
+
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await browser.url(`https://the-internet.herokuapp.com/login`)
+
+        await $('#username').setValue('tomsmith')
+        await $('#password').setValue('SuperSecretPassword!')
+        await $('button[type="submit"]').click()
+
+        await expect($('#flash')).toBeExisting()
+        await expect($('#flash')).toHaveText(
+            expect.stringContaining('You logged into a secure area!'))
+    })
+})
+
---
+++ test/specs/webview.e2e.js
@@ -0,0 +1,51 @@
+//var assert = require('assert');
+
+import { expect, browser, $ } from '@wdio/globals'
+
+describe('Scroll issue webview', () => {
+  it('fidn element and scroll', async () => {
+
+    const vendorsElt = await $('android=new UiSelector().text("Vendors:")');
+    
+    await vendorsElt.waitForDisplayed({ timeout: 5000 }); // Waits up to 5 seconds
+
+    var sortFilter = await $('android=new UiSelector().text("Select")');
+
+    //var filterBtn= await $('android=new UiSelector().text("Select")').scrollIntoView({ block: 'center', inline: 'nearest' });
+
+    //await driver.execute('mobile: scroll', { element: await $('android=new UiSelector().text("Select")'), direction: 'down' });
+
+    await driver.switchContext("WEBVIEW_com.browserstack.webviewtester");
+
+    //const element = await driver.$('//div[@id="1"]/div[2]/img');
+    //const element = await driver.$('//option[text()="Select"]');  /*Workign code*/
+    const element = await driver.$('//div[@id="1"]/p');  
+    await driver.executeScript("arguments[0].scrollIntoView();", [element]);
+
+    await driver.switchContext("NATIVE_APP");
+
+  //   await driver.execute('mobile: scroll', { 
+  //     element: await $('android=new UiSelector().text("Select")'), 
+  //     direction: 'down' 
+  // });
+
+
+    
+// New Try
+    var orderByBtn= await $('android=new UiSelector().text("iPhone 12")');
+
+    orderByBtn.click();
+
+
+
+  //Workign Code
+    /* await sortFilter.click();
+
+    var orderByBtn= await $('android=new UiSelector().text("Lowest to highest")');
+
+    var text = orderByBtn.getText();
+    console.log('Text:', text); */
+    
+    //assert(allProductsName.length > 0);
+  });
+});
